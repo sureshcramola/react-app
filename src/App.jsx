55a9e88cede9f972f88fcb9c2838b88e20cbf3d0
WorkStation <suresh.ramola@evontech.com>
2018-09-01 23:37:12 +0530

    contact page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,32 @@ class App extends Component {
       <div className="App">
         <Header/>
         <div className="main-content-wrapper">
+          <div className="container">
+            <h1 className="page-heading text-center">Contact Us</h1>
+            <div className="row">
+              <div className="col-xs-12 col-sm-6 col-md-6">
+                <div className="form-group">
+                  <label className="w-100">Name</label>
+                  <input type="text" name="name"/>
+                </div>
+                <div className="form-group">
+                  <label className="w-100">Email</label>
+                  <input type="text" name="email"/>
+                </div>
+                <div className="form-group">
+                  <label className="w-100">Phone Number</label>
+                  <input type="text" name="phone_number"/>
+                </div>
+                <div className="form-group">
+                  <label className="w-100">Message</label>
+                  <textarea rows="3"></textarea>
+                </div>
+              </div>
+              <div className="col-xs-12 col-sm-6 col-md-6">
+
+              </div>
+            </div>
+          </div>
         </div>
         <Footer/>
       </div>
